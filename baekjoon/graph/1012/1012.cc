e08d5de2f17d3725fa20d51dd2931c47ad56c409
ssgskid <ks8171235@naver.com>
2018-03-09 02:42:32 -0800

    1012

--- baekjoon/graph/1012/1012.cc
+++ baekjoon/graph/1012/1012.cc
@@ -4,18 +4,48 @@
 
 using namespace std;
 
+vector< vector<bool> > node;
+
+int N, M, K;
+
+void dfs(int i, int j){
+	node[i][j] = false;
+	if(i<(N-1)){
+		if (node[i+1][j]) dfs(i+1, j);
+	}
+	if(i>0){
+		if(node[i-1][j]) dfs(i-1, j);
+	}
+	if(j<(M-1)){
+	 	if(node[i][j+1]) dfs(i, j+1);
+	}
+	if(j>0){
+		if (node[i][j-1]) dfs(i, j-1);
+	}
+	return;
+}
+
 int main(void)
 {
-	vector< vector<bool> > node;
-	int N, M, K;
 	int n,m;
+	int test;
+	int count=0;
 
+	cin >> test;
+	
+	while(test--){
+	
+	count =0;
 	cin >> N >> M >> K;
 	
 	node.resize(N);
 	for(int i=0;i<N;i++){
 		node[i].resize(M);
 	}
+
+	for(int i=0;i<N;i++)
+		for(int j=0;j<M;j++)
+			node[i][j] = false;
 
 	for(int i=0;i<K;i++){
 		cin >> n >> m;
@@ -24,9 +54,14 @@ int main(void)
 
 	for(int i=0;i<N;i++){
 		for(int j=0;j<M;j++){
-			cout << node[N][M];
+			if(node[i][j]){
+			 	dfs(i, j);
+				count++;
+			}
 		}
-		cout << "\n";
+	}
+
+	cout << count << '\n';
 	}
 	return 0;
 }
